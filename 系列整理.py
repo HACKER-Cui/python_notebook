b25e5d869753c2f0e7883f4c4ba53f0c0f2d8c0f
HACKER-Cui <110284510+HACKER-Cui@users.noreply.github.com>
2025-02-11 21:00:56 +0800

    Update 系列整理.py

--- 系列整理.py
+++ 系列整理.py
@@ -6,3 +6,26 @@
 2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
 7 也是一个候选， 7 = 7 。
 仅有这两种组合。
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        path=[]
+        result=[]
+        #set1=set()
+        def backtrack(start,target):
+            if sum(path)==target:
+                result.append(path[:])
+                return
+            for i in range(start,len(candidates)):
+                #print(i,target)       
+                print(path)
+                if sum(path)>target:
+                    return 
+                path.append(candidates[i])
+                backtrack(i,target)
+                path.pop()
+        backtrack(0,target)
+        return result

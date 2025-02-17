33db168576497b8fea11347382d0edbb2c894bc8
Cui <110284510+HACKER-Cui@users.noreply.github.com>
2025-02-17 20:07:34 +0800

    Update 常见代码框架总结.py

--- 常见代码框架总结.py
+++ 常见代码框架总结.py
@@ -31,4 +31,45 @@ while que:
 0 1 2 3 4 5
 2取0 和2 str[]
 3取1 和2 
-4
+4递归思考架构
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result = []
+        def backtrack(s, left, right, depth=0):
+            # Python 2 compatible string formatting
+            print("|  " * depth + "'{}' L:{} R:{}".format(s, left, right),"deep=",depth)          
+            if len(s) == 2 * n:
+                result.append(s)
+                return
+            if left < n:
+                backtrack(s + '(', left + 1, right, depth + 1)
+            if left > right:
+                backtrack(s + ')', left, right + 1, depth + 1)               
+        backtrack("", 0, 0)
+        return result
+("'' L:0 R:0", 'deep=', 0)
+("|  '(' L:1 R:0", 'deep=', 1)
+("|  |  '((' L:2 R:0", 'deep=', 2)
+("|  |  |  '(((' L:3 R:0", 'deep=', 3)
+("|  |  |  |  '((()' L:3 R:1", 'deep=', 4)
+("|  |  |  |  |  '((())' L:3 R:2", 'deep=', 5)
+("|  |  |  |  |  |  '((()))' L:3 R:3", 'deep=', 6)
+("|  |  |  '(()' L:2 R:1", 'deep=', 3)
+("|  |  |  |  '(()(' L:3 R:1", 'deep=', 4)
+("|  |  |  |  |  '(()()' L:3 R:2", 'deep=', 5)
+("|  |  |  |  |  |  '(()())' L:3 R:3", 'deep=', 6)
+("|  |  |  |  '(())' L:2 R:2", 'deep=', 4)
+("|  |  |  |  |  '(())(' L:3 R:2", 'deep=', 5)
+("|  |  |  |  |  |  '(())()' L:3 R:3", 'deep=', 6)
+("|  |  '()' L:1 R:1", 'deep=', 2)
+("|  |  |  '()(' L:2 R:1", 'deep=', 3)
+("|  |  |  |  '()((' L:3 R:1", 'deep=', 4)
+("|  |  |  |  |  '()(()' L:3 R:2", 'deep=', 5)
+("|  |  |  |  |  |  '()(())' L:3 R:3", 'deep=', 6)
+("|  |  |  |  '()()' L:2 R:2", 'deep=', 4)
+("|  |  |  |  |  '()()(' L:3 R:2", 'deep=', 5)
+("|  |  |  |  |  |  '()()()' L:3 R:3", 'deep=', 6)

da0d91e88212676fb2701345b9561addfc25ff71
Cui <110284510+HACKER-Cui@users.noreply.github.com>
2025-02-20 19:52:06 +0800

    Update 系列整理.py

--- 系列整理.py
+++ 系列整理.py
@@ -29,3 +29,22 @@
                 path.pop()
         backtrack(0,target)
         return result
+3二分查找
+比如，左闭右闭区间和左闭右开区间的区别。在左闭右闭区间中，初始的right是数组的最后一个元素的索引，因此循环条件应该是left <= right，因为当left等于right时，区间仍然有效。而在左闭右开区间中，right是数组长度，此时循环条件应为left < right，因为当left等于right时，区间已经无效了。 然后，中间值的计算。通常用mid = (left + right) // 2，但在left和right很大的时候可能会溢出，所以更安全的写法是mid = left + (right - left) // 2。
+二分查找的边界条件可通过统一区间定义和循环不变量来理清思路：
+
+    区间定义（二选一）：
+
+    左闭右闭 [left, right] → 终止条件 left > right
+    左闭右开 [left, right) → 终止条件 left == right
+
+    万能模板（左闭右闭）：
+
+left, right = 0, len(nums)-1
+while left <= right:
+    mid = left + (right-left)//2  # 防溢出
+    if nums[mid] < target:
+        left = mid + 1   # 搜索[mid+1, right]
+    else:
+        right = mid -1   # 搜索[left, mid-1]
+return left  # 首个>=target的位置

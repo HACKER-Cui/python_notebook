85a9f973aafd412d82fb580a8c63c4a2b005ebc6
Cui <110284510+HACKER-Cui@users.noreply.github.com>
2025-02-26 16:17:30 +0800

    Update 常见python内置函数总结.py

--- 常见python内置函数总结.py
+++ 常见python内置函数总结.py
@@ -1,7 +1,19 @@
 1、翻转函数
 s[i:i+k]=reversed(s[i:k+i])
-2、列表转字符串函数
+2、join() 列表转字符串函数
 s="".join(s)
+str1="ab,sca"
+list1=list(str1)
+print(list1)
+b="".join(list1)
+c=" ".join(list1)
+print(b)
+print(c)
+结果：
+['a', 'b', ',', 's', 'c', 'a']
+ab,sca
+a b , s c a
+
 3、list1.reverse()是在list1原地修改，没有返回值
 4、for i num in enumerate(nums):取出位置与值
 5、ord()

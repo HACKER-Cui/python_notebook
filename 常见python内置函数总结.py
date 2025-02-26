cd4110ef17b377cf069a1558efd23335aa28eaf8
Cui <110284510+HACKER-Cui@users.noreply.github.com>
2025-02-26 15:57:47 +0800

    Update 常见python内置函数总结.py

--- 常见python内置函数总结.py
+++ 常见python内置函数总结.py
@@ -4,3 +4,10 @@ s[i:i+k]=reversed(s[i:k+i])
 s="".join(s)
 3、list1.reverse()是在list1原地修改，没有返回值
 4、for i num in enumerate(nums):取出位置与值
+5、ord()
+字符转asc码 如print(ord("1")-ord("0"))求某字符数字的值
+6、split("x")
+以某字符作为分隔符把字符串转成list
+str1="ab,sca"
+print(list(str1))                 ['a', 'b', ',', 's', 'c', 'a']
+print(str1.split(","))             ['ab', 'sca']
